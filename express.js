9adec230aa7f02938c8b7c1e5aed9b472cece80b
kendall <kpaige.henderson@gmail.com>
2024-03-21 18:04:18 -0600

    fixed errors and bugs

--- express.js
+++ express.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-//const 
 
 //import express and node js
 const express = require('express');
@@ -10,27 +9,69 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//landing page route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 //notes page route
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Interal server error' });
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 
 
+app.post('/api/notes', (req, res) => {
+
+    const { title, text } = req.body;
+    if (!title || !text) {
+        res.status(400).json({ error: 'Title and Text are returned' });
+        return;
+    }
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Interal server error' });
+            return;
+        }
+
+        const notes = JSON.parse(data);
+        const newNote = { id: generateUniqueId(), title, text };
+        notes.push(newNote);
+        console.log(newNote);
+
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Interal server error' });
+                return;
+            }
+            res.json(newNote);
+        });
+    });
+});
+
+function generateUniqueId() {
+ return Math.floor(Math.random() * 1000);
+}
+
+//landing page route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
